c596532edc4e9a592598e00c26a2859ec2562fdd
Mehmet Kaan KARAGÖZOĞLU <55835876+Dtractus@users.noreply.github.com>
2024-03-05 01:06:16 +0300

    Update NotFound.js

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,11 @@
-// src/components/NotFound.js
-import React from 'react';
+// At the top of your NotFound.js file
+import './NotFound.css';
 
 function NotFound() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>404 - Page Not Found</h1>
-      <p>Oops! The page you are looking for does not exist.</p>
+    <div className="not-found-container">
+      <h1 className="not-found-title">404 - Page Not Found</h1>
+      <p className="not-found-text">Oops! The page you are looking for does not exist.</p>
     </div>
   );
 }
